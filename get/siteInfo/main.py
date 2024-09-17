e618cdfe884ba2688c9e186f1557682896a6f883
LYXOfficial <lyx2010official@outlook.com>
2024-09-17 13:54:26 +0800

    add imagetoken

--- get/siteInfo/main.py
+++ get/siteInfo/main.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter,Depends,HTTPException
+from fastapi import APIRouter,Depends,HTTPException,Header
 import motor.motor_asyncio as motor
-import os
-
+import os,jwt
+SECRET_KEY=os.environ.get("SECRET")
+ALGORITHM="HS256"
 app=APIRouter()
 
 async def getDb():
@@ -15,3 +16,18 @@ async def getLatestUpdateTime(currentCollection=Depends(getDb)):
         return {"message":"success","time":time}
     except Exception as e:
         raise HTTPException(status_code=500,detail={"message":"fail","error":str(e)})
+
+async def verify(authorization: str=Header(None)):
+    if not authorization:
+        raise HTTPException(status_code=401,detail="Authorization header missing")
+    token=authorization.split(" ")[1] if " " in authorization else authorization
+    try:
+        jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+    except Exception as e:
+        raise HTTPException(status_code=401,detail="Invalid token")
+@app.get("/imageUploadToken")
+async def imageUploadToken(user=Depends(verify)):
+    try:
+        return {"message":"success","token":os.environ.get("QBU_TOKEN")}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail={"message": "fail", "error": str(e)})
