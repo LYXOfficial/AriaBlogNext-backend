dfdf2e96ff3142583119207b13099f097a01eb8a
LYXOfficial <lyx2010official@outlook.com>
2024-09-20 11:24:02 +0800

    add updatespeaks

--- update/speaks/main.py
+++ update/speaks/main.py
@@ -1,12 +1,20 @@
 from fastapi import APIRouter,Depends,HTTPException,Header
 import motor.motor_asyncio as motor
-import os,jwt
+import os,jwt,re
 from pydantic import BaseModel
 SECRET_KEY=os.environ.get("SECRET")
 ALGORITHM="HS256"
 app=APIRouter()
 class DeleteSpeaksRequestBody(BaseModel):
     time:int
+class UpdateSpeaksRequestBody(BaseModel):
+    time:int
+    content:str
+def replace_and_remove_tags(text):
+    text=re.sub(r'<a\s+[^>]*>.*?<\/a>','[链接]',text)
+    text=re.sub(r'<img\s+[^>]*>','[图片]',text)
+    text=re.sub('<.*?>','',text)
+    return text
 async def getDb():
     mongoClient=motor.AsyncIOMotorClient(os.environ.get("MONGODB_URI") or "mongodb://localhost:27017")
     return mongoClient["AriaBlogNext"]["Speaks"]
@@ -19,9 +27,23 @@ async def verify(authorization: str=Header(None)):
     except Exception as e:
         raise HTTPException(status_code=401,detail="Invalid token")
 @app.delete("/deleteSpeaks")
-async def deleteSpeaks(body:DeleteSpeaksRequestBody,currentCollection=Depends(getDb)):
+async def deleteSpeaks(body:DeleteSpeaksRequestBody,currentCollection=Depends(getDb),user=Depends(verify)):
     try:
         await currentCollection.delete_one({"time":body.time})
+        return {"message":"success"}
+    except Exception as e:
+        raise HTTPException(status_code=500,detail={"message":"fail","error":str(e)})
+@app.put("updateSpeaks")
+async def updateSpeaks(body:UpdateSpeaksRequestBody,currentCollection=Depends(getDb),user=Depends(verify)):
+    try:
+        await currentCollection.update_one({"time":body.time},{"$set":{"content":body.content,"plainContent":replace_and_remove_tags(body.content)}})
+        return {"message":"success"}
+    except Exception as e:
+        raise HTTPException(status_code=500,detail={"message":"fail","error":str(e)})
+@app.post("newSpeaks")
+async def newSpeaks(body:UpdateSpeaksRequestBody,currentCollection=Depends(getDb),user=Depends(verify)):
+    try:
+        await currentCollection.insert_one({"time":body.time},{"content":body.content,"plainContent":replace_and_remove_tags(body.content)})
         return {"message":"success"}
     except Exception as e:
         raise HTTPException(status_code=500,detail={"message":"fail","error":str(e)})
