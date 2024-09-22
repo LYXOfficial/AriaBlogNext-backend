ab43d245f113ee005b0d36eec13646c06b0ff755
LYXOfficial <lyx2010official@outlook.com>
2024-09-22 20:55:53 +0800

    add uploadimage

--- get/siteInfo/main.py
+++ get/siteInfo/main.py
@@ -1,8 +1,6 @@
-from fastapi import APIRouter,Depends,HTTPException,Header
+from fastapi import APIRouter,Depends,HTTPException
 import motor.motor_asyncio as motor
-import os,jwt
-SECRET_KEY=os.environ.get("SECRET")
-ALGORITHM="HS256"
+import os
 app=APIRouter()
 
 async def getDb():
@@ -16,18 +14,3 @@ async def getLatestUpdateTime(currentCollection=Depends(getDb)):
         return {"message":"success","time":time}
     except Exception as e:
         raise HTTPException(status_code=500,detail={"message":"fail","error":str(e)})
-
-async def verify(authorization: str=Header(None)):
-    if not authorization:
-        raise HTTPException(status_code=401,detail="Authorization header missing")
-    token=authorization.split(" ")[1] if " " in authorization else authorization
-    try:
-        jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
-    except Exception as e:
-        raise HTTPException(status_code=401,detail="Invalid token")
-@app.get("/imageUploadToken")
-async def imageUploadToken(user=Depends(verify)):
-    try:
-        return {"message":"success","token":os.environ.get("QBU_TOKEN")}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail={"message": "fail", "error": str(e)})
---
+++ update/image/main.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter,Depends,HTTPException,File,Header
+import motor.motor_asyncio as motor
+import os,jwt,httpx
+SECRET_KEY=os.environ.get("SECRET")
+ALGORITHM="HS256"
+QBU_TOKEN=os.environ.get("QBU_TOKEN")
+app=APIRouter()
+async def verify(authorization: str=Header(None)):
+    if not authorization:
+        raise HTTPException(status_code=401,detail="Authorization header missing")
+    token=authorization.split(" ")[1] if " " in authorization else authorization
+    try:
+        jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+    except Exception as e:
+        raise HTTPException(status_code=401,detail="Invalid token")
+async def upload(url,body,headers):
+    async with httpx.AsyncClient() as client:
+        response=await client.post(url,body=body,headers=headers)
+        return response.json()
+@app.post("/uploadImage")
+async def uploadImage(file:File,user=Depends(verify)):
+    try:
+        headers={
+            "Content-Type":"multipart/form-data",
+            "Authorization":QBU_TOKEN,
+            "Accept":"application/json",
+        }
+        body={"file":file}
+        return {"message":"success","data":await upload("https://7bu.top/api/v1/upload",body,headers)}
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500,detail={"message":"fail","error":str(e)})
